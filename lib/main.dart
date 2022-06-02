3cdc9f1c167f5d8aee5553087cb4fbe95036e2d6
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2022-06-02 16:51:31 +0200

    Add the navigation towards a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Welcome to Flutter",
-        home: Scaffold(
-          appBar: AppBar(
-              title: const Text("Welcome to Flutter")
-          ),
-          body: const Center(
-            child: RandomWords(),
-          ),
-        )
-    );
+    return const MaterialApp(
+        title: "Startup Name Generator", home: RandomWords());
   }
 }
 
@@ -39,43 +29,76 @@ class _RandomWordsState extends State<RandomWords> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) {
-          return const Divider();
-        }
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Startup Name Generator"),
+          actions: [
+            IconButton(
+              onPressed: _pushSaved,
+              icon: const Icon(Icons.list),
+              tooltip: "Saved suggestions",
+            )
+          ],
+        ),
+        body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) {
+              return const Divider();
+            }
 
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
 
-        final suggestion = _suggestions[index];
+            final suggestion = _suggestions[index];
 
-        final alreadySaved = _saved.contains(suggestion);
+            final alreadySaved = _saved.contains(suggestion);
 
-        return ListTile(
-          title: Text(
-            suggestion.asCamelCase,
-            style: _biggerFont,
-          ),
-          trailing: Icon(
-            alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Colors.red : null,
-            semanticLabel: alreadySaved ? "Removed from saved" : "Save",
-          ),
-          onTap: () {
-            setState(() {
-              if (alreadySaved) {
-                _saved.remove(suggestion);
-              } else {
-                _saved.add(suggestion);
-              }
-            });
+            return ListTile(
+              title: Text(
+                suggestion.asCamelCase,
+                style: _biggerFont,
+              ),
+              trailing: Icon(
+                alreadySaved ? Icons.favorite : Icons.favorite_border,
+                color: alreadySaved ? Colors.red : null,
+                semanticLabel: alreadySaved ? "Removed from saved" : "Save",
+              ),
+              onTap: () {
+                setState(() {
+                  if (alreadySaved) {
+                    _saved.remove(suggestion);
+                  } else {
+                    _saved.add(suggestion);
+                  }
+                });
+              },
+            );
           },
-        );
-      },
-    );
+        ));
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return ListTile(
+            title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ));
+      });
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(tiles: tiles, context: context).toList()
+          : <Widget>[];
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Saved suggestions"),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 }
