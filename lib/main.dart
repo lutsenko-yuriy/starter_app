3b38f2f1a93c9b2b9e356924aa3f95b7dca7e1fa
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2022-06-02 15:33:41 +0200

    Make the items clickable

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Welcome to Flutter",
-      home: Scaffold(
-        appBar: AppBar(
-            title: const Text("Welcome to Flutter")
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      )
+        title: "Welcome to Flutter",
+        home: Scaffold(
+          appBar: AppBar(
+              title: const Text("Welcome to Flutter")
+          ),
+          body: const Center(
+            child: RandomWords(),
+          ),
+        )
     );
   }
 }
@@ -36,35 +36,46 @@ class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
-  
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) {
-            return const Divider();
-          }
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
 
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
 
-          final alreadySaved = _saved.contains(_suggestions[index]);
+        final suggestion = _suggestions[index];
 
-          return ListTile(
-            title: Text(
-              _suggestions[index].asCamelCase,
-              style: _biggerFont,
-            ),
-            trailing: Icon(
-              alreadySaved ? Icons.favorite : Icons.favorite_border,
-              color: alreadySaved ? Colors.red : null,
-              semanticLabel: alreadySaved ? "Removed from saved" : "Save",
-            ),
-          );
-        },
+        final alreadySaved = _saved.contains(suggestion);
+
+        return ListTile(
+          title: Text(
+            suggestion.asCamelCase,
+            style: _biggerFont,
+          ),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? "Removed from saved" : "Save",
+          ),
+          onTap: () {
+            setState(() {
+              if (alreadySaved) {
+                _saved.remove(suggestion);
+              } else {
+                _saved.add(suggestion);
+              }
+            });
+          },
+        );
+      },
     );
   }
 }
